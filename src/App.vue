152eeac275cf3ee43c071e8868dff50edf909c2c
oldboydevelop <70367618+oldboydevelop@users.noreply.github.com>
2026-01-26 10:41:50 +0200

    feat: integrate CircularProgress component into App.vue with initial properties

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CircularProgress from './components/CircularProgress.vue'
+</script>
 
 <template>
-  <div></div>
+  <CircularProgress
+    :value="70"
+    :max="150"
+    :size="120"
+    :strokeWidth="10"
+    color="#3498db"
+    type="circle"
+  />
 </template>
 
 <style scoped></style>
