f50c1aa1ee8f5ac9e6498e82a1453f735ede2ebe
oldboydevelop <70367618+oldboydevelop@users.noreply.github.com>
2026-01-27 14:14:39 +0200

    feat: enhance ControlField component with styling and layout, and add CircularProgress examples in demo page

--- src/components/ControlField.vue
+++ src/components/ControlField.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="control-field">
-    <label :for="id">{{ label }}</label>
-    <input :id="id" :type="type" v-model="model" :min="min" :max="max" />
+    <label class="label" :for="id">{{ label }}</label>
+    <input class="custom-input" :id="id" :type="type" v-model="model" :min="min" :max="max" />
   </div>
 </template>
 
 <script setup lang="ts">
 defineProps<{
   label: string
+  placeholder?: string
   id?: string
   type?: string
   min?: number
@@ -16,4 +17,20 @@ defineProps<{
 const model = defineModel<string | number>('modelValue')
 </script>
 
-<style scoped></style>
+<style scoped>
+.control-field {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+}
+.label {
+  font-size: 18px;
+  font-weight: 500;
+}
+.custom-input {
+  max-width: 250px;
+  padding: 4px 6px;
+  border: 1px solid #d1d5db;
+  border-radius: 4px;
+}
+</style>
--- src/pages/CircularProgressDemo.vue
+++ src/pages/CircularProgressDemo.vue
@@ -1,16 +1,49 @@
 <template>
-  <ControlField v-model="model.name" label="Наименование" />
+  <div class="page">
+    <div class="demo-grid">
+      <CircularProgress :value="0" :max="100" :size="100" :stroke-width="10" type="circle" />
+      <CircularProgress :value="25" :max="100" :size="100" :stroke-width="10" type="circle" />
+      <CircularProgress
+        :value="100"
+        :max="100"
+        :size="100"
+        status="success"
+        :stroke-width="10"
+        type="circle"
+      />
+      <CircularProgress
+        :value="50"
+        :max="100"
+        :size="100"
+        status="warning"
+        :stroke-width="10"
+        type="circle"
+      />
+      <CircularProgress
+        :value="50"
+        :max="100"
+        :size="100"
+        status="error"
+        :stroke-width="10"
+        type="circle"
+      />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import ControlField from '@/components/ControlField.vue'
-import { reactive } from 'vue'
-
-const model = reactive({
-  name: '',
-  value: 0,
-  color: '',
-})
+import CircularProgress from '@/components/CircularProgress.vue'
 </script>
 
-<style scoped></style>
+<style scoped>
+.demo-grid {
+  display: grid;
+  justify-content: center;
+  grid-template-columns: repeat(auto-fit, minmax(120px, 1fr));
+  gap: 24px;
+}
+.page {
+  max-width: 1280px;
+  margin: 0 auto;
+}
+</style>
