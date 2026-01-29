e05c03c9641c9d003729c21dae2ec62042e95e75
oldboydevelop <70367618+oldboydevelop@users.noreply.github.com>
2026-01-29 11:42:59 +0200

    feat: add PieChart component and update dependencies for chart.js and vue-chartjs

---
+++ src/components/PieChart.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="pie-chart-wrapper">
+    <canvas ref="canvasRef"></canvas>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Chart, PieController, ArcElement, Tooltip, Legend } from 'chart.js'
+Chart.register(PieController, ArcElement, Tooltip, Legend)
+</script>
+
+<style scoped></style>
