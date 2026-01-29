a1bbe18288dac6f5fc6456039f9c36ceae4671b5
oldboydevelop <70367618+oldboydevelop@users.noreply.github.com>
2026-01-29 11:55:55 +0200

    feat: implement PieChart component with dynamic data handling and chart initialization

--- src/components/PieChart.vue
+++ src/components/PieChart.vue
@@ -5,8 +5,57 @@
 </template>
 
 <script setup lang="ts">
+import type { Sector } from '@/types'
 import { Chart, PieController, ArcElement, Tooltip, Legend } from 'chart.js'
+import { ref, watch } from 'vue'
 Chart.register(PieController, ArcElement, Tooltip, Legend)
+
+const props = defineProps<{
+  sectors: Sector[]
+}>()
+
+const canvasRef = ref<HTMLCanvasElement | null>(null)
+const chart = ref<Chart | null>(null)
+
+const getChartData = () => ({
+  labes: props.sectors.map((sector) => sector.name),
+  datasets: [
+    {
+      data: props.sectors.map((s) => s.value),
+      backgroundColor: props.sectors.map((s) => s.color),
+    },
+  ],
+})
+
+const initChart = () => {
+  if (!canvasRef.value) return
+  chart.value = new Chart(canvasRef.value, {
+    type: 'pie',
+    data: getChartData(),
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'bottom',
+        },
+        tooltip: {
+          enabled: true,
+        },
+      },
+    },
+  })
+}
+
+watch(
+  () => props.sectors,
+  () => {
+    if (chart.value) {
+      chart.value.data = getChartData()
+      chart.value.update()
+    }
+  },
+  { deep: true },
+)
 </script>
 
 <style scoped></style>
